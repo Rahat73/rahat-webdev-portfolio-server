63766753e0dd5ee0058df30ecde1fb350b25c0ed
Rahat73 <rahat.ashik.18@gmail.com>
2024-12-26 15:51:54 +0600

    deploy

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'https://rahat-webdev-portfolio-dashboard.vercel.app',
+      'https://rahat-webdev.netlify.app',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
     credentials: true,
   }),
 );
